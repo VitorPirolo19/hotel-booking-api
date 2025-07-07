ec6f20c8f6892c895df896881b10bda216ed113a
VitorPirolo19 <vpirolo10@gmail.com>
2025-07-07 20:14:04 -0300

    build: seed H2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO rooms(number, room_type, capacity_of_people, price_per_night) VALUES
+(10, 'SINGLE', 1, 100.00),
+(15, 'SUITE', 3,300.00);
+
+INSERT INTO bookings(id, room_number, username, number_of_people, checkin, checkout) VALUES
+(1, 10, 'vitor', 1, '2025-07-12T12:00', '2025-07-29T12:00');
