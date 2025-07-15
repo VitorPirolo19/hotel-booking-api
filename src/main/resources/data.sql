a51d6df5d443e001354ac0c5d82ae85fda1fa907
VitorPirolo19 <vpirolo10@gmail.com>
2025-07-15 13:57:51 -0300

    build: remove the id field on booking SQL insert

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO rooms(number, room_type, capacity_of_people, price_per_night) VALUES
 (10, 'SINGLE', 1, 100.00),
 (15, 'SUITE', 3,300.00);
 
-INSERT INTO bookings(id, room_number, username, number_of_people, checkin, checkout) VALUES
-(1, 10, 'vitor', 1, '2025-07-12T12:00', '2025-07-29T12:00'),
-(2, 15, 'anna', 1, '2025-08-01T12:00', '2025-08-15T16:00');
+INSERT INTO bookings(room_number, username, number_of_people, checkin, checkout) VALUES
+(10, 'vitor', 1, '2025-07-12T12:00:00', '2025-07-29T12:00:00'),
+(15, 'anna', 1, '2025-08-01T12:00:00', '2025-08-15T16:00:00');
