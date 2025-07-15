99bd75ea5c280e090bd26869fbbc92ca520d20d3
VitorPirolo19 <vpirolo10@gmail.com>
2025-07-15 13:56:25 -0300

    feat: add checking and checkout date and quantity of people validation to create a new booking

--- src/main/java/com/hotel/booking/service/BookingService.java
+++ src/main/java/com/hotel/booking/service/BookingService.java
@@ -1,6 +1,8 @@
 package com.hotel.booking.service;
 
 import com.hotel.booking.entity.Booking;
+import com.hotel.booking.exception.InvalidDateException;
+import com.hotel.booking.exception.InvalidNumberOfPeopleException;
 import com.hotel.booking.repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,11 @@ public class BookingService {
     private BookingRepository bookingRepository;
 
     public Booking saveBooking(Booking booking){
+        if (booking.getCheckin().isAfter(booking.getCheckout())){
+            throw new InvalidDateException("check-in cannot be after check-out");
+        } else if (booking.getRoom().getCapacity_of_people() < booking.getNumber_of_people()) {
+            throw new InvalidNumberOfPeopleException("the number of people cannot be bigger than the room capacity");
+        }
         return bookingRepository.save(booking);
     }
 
